54c92a50649ac0b2f2b9e941ad0ff6e5c42f1c1c
bharathviswam <32975209+bharathviswam@users.noreply.github.com>
2020-08-13 20:22:14 +0530

    Update chatbot.sh

--- chatbot.sh
+++ chatbot.sh
@@ -14,3 +14,16 @@ then
 
         curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$output\"}" https://hooks.slack.com/services/T018BGFM3M4/B018Z0L6EF6/$2
 fi
+
+if [ "$command" == *'delete pod'* ]
+then
+        echo "Executing OC Commands"
+        ./oc login --token=Ns9Q0zEUDSvDs-Guyp60uUIoEvNXUmOBMHeQv1IpSAE --server=https://c100-e.eu-gb.containers.cloud.ibm.com:30450
+
+        ./oc project i2devops
+        output=`./oc $command`
+
+        echo "Sending the Result to Slack"
+
+        curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$output\"}" https://hooks.slack.com/services/T018BGFM3M4/B018Z0L6EF6/$2
+fi
